7d1cb1233e5e186d3898b44ad421ba3573d70ea7
Faizan <faizanswabi95@gmail.com>
2025-02-25 01:27:11 +0500

    Update LandingPage.js

--- Source/UI/src/pages/LandingPage.js
+++ Source/UI/src/pages/LandingPage.js
@@ -221,7 +221,7 @@ export default function LandingPage() {
         <h2 className="text-4xl font-bold text-teal-700 mb-6">
           Other Services
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="p-6 bg-white rounded-xl shadow-lg">
             <h3 className="text-2xl font-semibold">Image Segmentation</h3>
             <p className="mt-2 text-gray-600">
@@ -234,12 +234,7 @@ export default function LandingPage() {
               Comprehensive analytical tools for medical datasets.
             </p>
           </div>
-          <div className="p-6 bg-white rounded-xl shadow-lg">
-            <h3 className="text-2xl font-semibold">Consultation Services</h3>
-            <p className="mt-2 text-gray-600">
-              Expert consultations on AI integration in healthcare.
-            </p>
-          </div>
+          
         </div>
       </section>
 
